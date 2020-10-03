5ed3362c2fe842a3c8433569009cc90e32fb216c
ilia92 <ilia_92@mail.bg>
2020-10-04 01:05:58 +0300

    scripts update

--- home_navi/soft/cmd_exec.sh
+++ home_navi/soft/cmd_exec.sh
@@ -5,6 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cp -rpv $DIR/amdcovc /usr/bin/
 cp -rpv $DIR/amdvbflash /usr/bin/
+
 cp -rpv $DIR/rocm-smi /usr/bin/
+cp -rpv $DIR/rocm_smi.py /usr/bin/
+cp -rpv $DIR/rocminfo /usr/bin/
+
 cp -rpv $DIR/pbec /usr/bin/
 cp -rpv $DIR/amdmemtweak /usr/bin/
